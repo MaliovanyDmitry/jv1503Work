0686a13b60d2fd16a9cd9213f65fbc0b028aa769
MaliovanyDmitry <maliovany@gmail.com>
2015-11-24 23:44:07 +0200

    Lesson4 HomeTask

---
+++ BasicTypes/src/main/java/com/mycompany/basictypes/App.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.basictypes;
+
+
+public class App {
+    public static void main(String[] args) {
+       double x = 5.64329087;
+       int num = 10^n;
+       int n = 3;
+       int intx = (int)(x * num);
+       double resique = x - (intx * (10^-n));
+       if (resique > (5*10^-n))
+           double finalResique =  
+       
+       
+    }
+   
+}
