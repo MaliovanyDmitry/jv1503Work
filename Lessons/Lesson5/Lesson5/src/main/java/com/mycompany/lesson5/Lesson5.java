fd47d624f4c18369ee746364bc1b0fea42213b9e
MaliovanyDmitry <maliovany@gmail.com>
2015-11-28 19:30:40 +0200

    l5 First commit

---
+++ Lessons/Lesson5/Lesson5/src/main/java/com/mycompany/lesson5/Lesson5.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.lesson5;
+
+public class Lesson5 {
+    public static void main(String[] args) {
+      int a = 0;
+      int b = 3;
+      /*int c = a++;
+      int d = ++b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);*/
+     if(metod(a,b)>0)
+     
+      
+         } 
+    
+    public static int metod(int a, int b){
+        return (a-b);
+    }
+    
+}
