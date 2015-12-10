a7070822bdbd946fb23bc042da9f7060600904c0
MaliovanyDmitry <maliovany@gmail.com>
2015-12-11 00:31:46 +0200

    Lesson9HT

--- HomeTasks/Lesson9/HomeTask9.1/src/main/java/uits/jv1503/hometask9/App.java
+++ HomeTasks/Lesson9/HomeTask9.1/src/main/java/uits/jv1503/hometask9/App.java
@@ -5,23 +5,30 @@ import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
-        MyArray testArray = new MyArray();
+//        MyArray testArray = new MyArray();
+//        for(int i = 0; i<10; i++){
+//            Object o = new Integer(i*5);
+//            testArray.put(o);
+//        }
+////        for(int i = 0; i < testArray.length(); i++){
+////        System.out.println(testArray.get(i));
+////        }
+//        for(int i = 0; i<10; i++){
+//            Object l = new Integer(i*3);
+//            testArray.put(l);
+//        }
+//        Arrays.sort(MyArray.array);
+//        for(int i = 0; i < testArray.length(); i++){
+//        System.out.println(testArray.get(i));}
+         MyArray testArray = new MyArray();
         for(int i = 0; i<10; i++){
             Object o = new Integer(i*5);
             testArray.put(o);
         }
-//        for(int i = 0; i < testArray.length(); i++){
-//        System.out.println(testArray.get(i));
-//        }
-        for(int i = 0; i<10; i++){
-            Object l = new Integer(i*3);
-            testArray.put(l);
-        }
-        Arrays.sort(MyArray.array);
-        for(int i = 0; i < testArray.length(); i++){
-        System.out.println(testArray.get(i));}
-
+MyArray test2 = new MyArray();
+for(int i = 0; i<10; i++)
+System.out.println(testArray.get(i));
         
-        }
+    }
     
 }
